50de13493a2aaca296926baa0b3a449db1f4105e
Namrta Sharma <namrta1997sharma@gmail.com>
2023-01-16 20:13:06 +0530

    updated

--- test.py
+++ test.py
@@ -1,7 +1,3 @@
 print("this is my test.py file") 
 print("changes made from git repo")
-<<<<<<< HEAD
 print("merge conflict")
-=======
-print("merge conflict error")
->>>>>>> 67f6ec1c0338fe32cf2ef446adbd4cb469c70ed3
